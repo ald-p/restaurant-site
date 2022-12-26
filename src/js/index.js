ba5a5d803ec19a2e2d3a973bf90940016404678e
Aldwin <aldwin.pag@gmail.com>
2022-12-26 18:37:49 -0500

    Moved HTML content to JS file for homepage

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,12 @@
 import '../styles/custombs.scss';
 import * as bootstrap from 'bootstrap';
 import '../styles/custom_styles.css';
-import home from "../js/home";
-import about from "../js/about";
-import contact from "../js/contact";
+import homeContent from "./home";
+import menu from "./menu";
+import contact from "./contact";
+
+homeContent();
+
+
+
+
